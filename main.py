6336d905a038d7ca44929dfe0771b2daf81883d3
Your Name <resplin5072@sdsu.edu>
2024-06-11 16:40:21 -0700

    adds all statMix output & updates readme

---
+++ main.py
@@ -0,0 +1,29 @@
+import os
+
+def execute_command(command_list):
+    command = " ".join(command_list)
+    
+    print(command)
+    os.system(command)
+
+def get_statmix_stats():
+    stats = ["hwe", "pop_structure", "sfs", "generic_stats", "fsc"]
+    statmix_path = os.path.join("/home/raya/Documents/Projects/hops_pipeline/statMix", "statmix.py")
+    vcf_path = "/home/raya/Documents/Projects/hops_pipeline/input_data/hops.vcf"
+    output_prefix = "hops"
+
+    command = [
+        "python3",  # python executable
+        statmix_path,
+        "--vcf",
+        vcf_path,
+        "--out-prefix",
+        output_prefix,
+        "--analyses"
+    ] + [f"{stat}" for stat in stats]
+
+    execute_command(command)
+
+
+if __name__ == "__main__":
+    get_statmix_stats()
